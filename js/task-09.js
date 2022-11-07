5a79da1af467c3864460fb35fe9f68ceb0589343
YuriiKabo <yourrocksmirnov@gmail.com>
2022-11-07 23:13:51 +0200

    w9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,15 @@
+const bodyColor = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const bodyColor = document.querySelector("body");
-const colorValue = document.querySelector(".color");
-const changeColorBtn = document.querySelector(".change-color");
-
 changeColorBtn.addEventListener("click", changeColor);
 
 function changeColor() {
-  bodyColor.style.backgroundColor = getRandomHexColor();
-  colorValue.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
 }
